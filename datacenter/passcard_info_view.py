d47efa4421fcea233130d89c17effefffca4a7b6
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-01-18 13:43:57 +0300

    added get() method to passcard

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -12,28 +12,26 @@ def format_duration(duration):
 
 
 def passcard_info_view(request, passcode):
-    active_passcards = Passcard.objects.filter(is_active=True)
-    for passcard in active_passcards:
-        name = passcard.owner_name
-        this_passcard_visits = []
-        passcard_all_visits = Visit.objects.filter(passcard__owner_name=name)
-        for visit in passcard_all_visits:
-            en_time = visit.entered_at
-            if not visit.leaved_at:
-                duration = visit.get_duration()
-            elif visit.leaved_at:
-                duration = visit.leaved_at - visit.entered_at
-            formated_duration = format_duration(duration)
-            is_strange = visit.is_visit_long()
-            this_passcard_visits.append(
-                {
-                    'entered_at': en_time,
-                    'duration': formated_duration,
-                    'is_strange': is_strange
-                },
-            )
-        context = {
-            'passcard': passcard,
-            'this_passcard_visits': this_passcard_visits
-        }
-        return render(request, 'passcard_info.html', context)
+    passcard = Passcard.objects.get(passcode=passcode)
+    this_passcard_visits = []
+    passcard_all_visits = Visit.objects.filter(passcard=passcard)
+    for visit in passcard_all_visits:
+        en_time = visit.entered_at
+        if not visit.leaved_at:
+            duration = visit.get_duration()
+        elif visit.leaved_at:
+            duration = visit.leaved_at - visit.entered_at
+        formated_duration = format_duration(duration)
+        is_strange = visit.is_visit_long()
+        this_passcard_visits.append(
+            {
+                'entered_at': en_time,
+                'duration': formated_duration,
+                'is_strange': is_strange
+            },
+        )
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
